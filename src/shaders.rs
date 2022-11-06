8cac388f85cbe52761b9b454016592e924f66c40
Roy Matero <roymatero@gmail.com>
2022-11-05 19:19:21 -0500

    initial vulkan setup

--- src/shaders.rs
+++ src/shaders.rs
@@ -1,31 +1,60 @@
+// pub mod vs {
+//     vulkano_shaders::shader! {
+//         ty: "vertex",
+//         src: "
+//             #version 450
+
+//             layout(location = 0) in vec3 position;
+//             layout(location = 1) in vec3 color;
+
+//             layout(location = 0) out vec3 out_color;
+
+//             layout(set = 0, binding = 0) uniform MVP_Data {
+//                 mat4 world;
+//                 mat4 view;
+//                 mat4 projection;
+//             } uniforms;
+
+//             void main() {
+//                 mat4 worldview = uniforms.view * uniforms.world;
+//                 gl_Position = uniforms.projection * worldview * vec4(position, 1.0);
+//                 out_color = color;
+//             }
+//             ",
+//             types_meta: {
+//                 use bytemuck::{Pod, Zeroable};
+
+//                 #[derive(Clone, Copy, Zeroable, Pod)]
+//             }
+//     }
+// }
+
+// pub mod fs {
+//     vulkano_shaders::shader! {
+//         ty: "fragment",
+//         src: "
+//             #version 450
+//             layout(location = 0) in vec3 in_color;
+
+//             layout(location = 0) out vec4 f_color;
+
+//             void main() {
+//                 f_color = vec4(in_color, 1.0);
+//             }
+//             "
+//     }
+// }
+
 pub mod vs {
     vulkano_shaders::shader! {
         ty: "vertex",
         src: "
-            #version 450
-            
-            layout(location = 0) in vec3 position;
-            layout(location = 1) in vec3 color;
+#version 450
+layout(location = 0) in vec3 position;
 
-            layout(location = 0) out vec3 out_color;
-
-            layout(set = 0, binding = 0) uniform MVP_Data {
-                mat4 world;
-                mat4 view;
-                mat4 projection;
-            } uniforms;
-
-            void main() {
-                mat4 worldview = uniforms.view * uniforms.world;
-                gl_Position = uniforms.projection * worldview * vec4(position, 1.0);
-                out_color = color;
-            }
-            ",
-            types_meta: {
-                use bytemuck::{Pod, Zeroable};
-
-                #[derive(Clone, Copy, Zeroable, Pod)]
-            }
+void main() {
+    gl_Position = vec4(position, 1.0);
+}"
     }
 }
 
@@ -33,14 +62,12 @@ pub mod fs {
     vulkano_shaders::shader! {
         ty: "fragment",
         src: "
-            #version 450
-            layout(location = 0) in vec3 in_color;
+#version 450
+layout(location = 0) out vec4 f_color;
 
-            layout(location = 0) out vec4 f_color;
-
-            void main() {
-                f_color = vec4(in_color, 1.0);
-            }
-            "
+void main() {
+    f_color = vec4(1.0, 0.0, 0.0, 1.0);
+}
+"
     }
 }
--- src/vertex.rs
+++ src/vertex.rs
@@ -1,28 +1,27 @@
 use bytemuck::{Pod, Zeroable};
-use nalgebra_glm::{identity, TMat4};
 
 #[repr(C)]
 #[derive(Clone, Copy, Debug, Default, Zeroable, Pod)]
 pub struct Vertex {
     pub position: [f32; 3],
-    pub color: [f32; 3],
+    // pub color: [f32; 3],
 }
 
-vulkano::impl_vertex!(Vertex, position, color);
+vulkano::impl_vertex!(Vertex, position);
 
-#[derive(Debug, Clone)]
-pub struct MVP {
-    pub model: TMat4<f32>,
-    pub view: TMat4<f32>,
-    pub projection: TMat4<f32>,
-}
+// #[derive(Debug, Clone)]
+// pub struct MVP {
+//     pub model: TMat4<f32>,
+//     pub view: TMat4<f32>,
+//     pub projection: TMat4<f32>,
+// }
 
-impl MVP {
-    pub fn new() -> MVP {
-        MVP {
-            model: identity(),
-            view: identity(),
-            projection: identity(),
-        }
-    }
-}
+// impl MVP {
+//     pub fn new() -> MVP {
+//         MVP {
+//             model: identity(),
+//             view: identity(),
+//             projection: identity(),
+//         }
+//     }
+// }
